c5371f7e39a8ed8883125b64d1f3c611ba1e6c8f
Ayman Mohamed <a.m_ayman9@outlook.com>
2023-12-25 10:08:04 +0200

    Solve Decode Ways Problem

---
+++ src/DecodeWays/Main.java
@@ -0,0 +1,10 @@
+package DecodeWays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.numDecodings("12"));  // Output: 2
+        System.out.println(solution.numDecodings("226")); // Output: 3
+        System.out.println(solution.numDecodings("06"));  // Output: 0
+    }
+}
